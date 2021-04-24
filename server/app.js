f23bbb0393de650f1a7c4d731fdc65e6da062129
kobi wolf <kobiwolf95@gmail.com>
2021-04-24 23:35:06 +0300

    'add-.env-file'

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require('./db/server.js');
 const express = require('express');
 const cors = require('cors');
--- server/db/server.js
+++ server/db/server.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-
-const path = process.env.URL_DB || 'mongodb://localhost:27017/bank-api';
+const path =
+  process.env.NODE_ENV !== 'development'
+    ? process.env.MONGO
+    : 'mongodb://localhost:27017/bank-api';
 
 mongoose.connect(path, {
   useNewUrlParser: true,
